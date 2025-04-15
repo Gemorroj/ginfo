bab967922ff3e3762ff477bb6a1cab88fed020dc
Gemorroj <wapinet@mail.ru>
2025-04-16 00:15:20 +0300

    fix: httpd parser

--- src/Parser/WebServer/Httpd.php
+++ src/Parser/WebServer/Httpd.php
@@ -83,8 +83,8 @@ final readonly class Httpd implements ParserInterface
             if (\str_contains($line, 'forked: ')) {
                 $res['forked'] = \str_contains($line, 'yes');
             }
-            if (\str_starts_with($line, ' -D ')) {
-                $res['args'] = $line;
+            if (\str_starts_with($line, '-D ')) {
+                $res['args'] .= $res['args'] ? ' '.$line : $line;
             }
         }
 
@@ -93,70 +93,70 @@ final readonly class Httpd implements ParserInterface
             if (false !== $statusPageContent) {
                 $statusPageLines = \explode("\n", \trim($statusPageContent));
                 foreach ($statusPageLines as $line) {
-                    if (\str_starts_with($line, '<dt>Server uptime: ')) {
-                        $res['uptime'] = \trim(\str_replace(['<dt>', '</dt>', 'Server uptime: '], '', $line));
+                    if (\str_starts_with($line, '<dt>Server uptime:')) {
+                        $res['status']['uptime'] = \trim(\str_replace(['<dt>', '</dt>', 'Server uptime:'], '', $line));
                     }
-                    if (\str_starts_with($line, '<dt>Server load: ')) {
-                        $res['load'] = \trim(\str_replace(['<dt>', '</dt>', 'Server load: '], '', $line));
+                    if (\str_starts_with($line, '<dt>Server load:')) {
+                        $res['status']['load'] = \trim(\str_replace(['<dt>', '</dt>', 'Server load:'], '', $line));
                     }
                     if (\str_contains($line, 'Total accesses:')) {
-                        if (\preg_match('/Total accesses: (\d+)/', $line, $matches)) {
-                            $res['total_accesses'] = (int) $matches[1];
+                        if (\preg_match('/Total accesses:\s+(\d+)/', $line, $matches)) {
+                            $res['status']['total_accesses'] = (int) $matches[1];
                         }
                     }
                     if (\str_contains($line, 'Total Traffic:')) {
-                        if (\preg_match('/Total Traffic: ([0-9.]+)\s+([a-zA-Z]+)/', $line, $matches)) {
-                            $res['total_traffic'] = ['value' => (float) $matches[1], 'unit' => $matches[2]];
+                        if (\preg_match('/Total Traffic:\s+([0-9.]+)\s+([a-zA-Z]+)/', $line, $matches)) {
+                            $res['status']['total_traffic'] = ['value' => (float) $matches[1], 'unit' => $matches[2]];
                         }
                     }
                     if (\str_contains($line, 'Total Duration:')) {
-                        if (\preg_match('/Total Duration: (\d+)/', $line, $matches)) {
-                            $res['total_duration'] = (int) $matches[1];
+                        if (\preg_match('/Total Duration:\s+(\d+)/', $line, $matches)) {
+                            $res['status']['total_duration'] = (int) $matches[1];
                         }
                     }
                     if (\str_contains($line, ' requests/sec')) {
                         if (\preg_match('/([0-9.]+)\s+requests\/sec/', $line, $matches)) {
-                            $res['requests_sec'] = (float) $matches[1];
+                            $res['status']['requests_sec'] = (float) $matches[1];
                         }
                     }
                     if (\str_contains($line, ' B/second')) {
                         if (\preg_match('/(\d+)\s+B\/second/', $line, $matches)) {
-                            $res['b_second'] = (int) $matches[1];
+                            $res['status']['b_second'] = (int) $matches[1];
                         }
                     }
                     if (\str_contains($line, ' B/request')) {
                         if (\preg_match('/(\d+)\s+B\/request/', $line, $matches)) {
-                            $res['b_request'] = (int) $matches[1];
+                            $res['status']['b_request'] = (int) $matches[1];
                         }
                     }
                     if (\str_contains($line, ' ms/request')) {
                         if (\preg_match('/([0-9.]+)\s+ms\/request/', $line, $matches)) {
-                            $res['ms_request'] = (float) $matches[1];
+                            $res['status']['ms_request'] = (float) $matches[1];
                         }
                     }
                     if (\str_contains($line, ' requests currently being processed')) {
                         if (\preg_match('/(\d+)\s+requests currently being processed/', $line, $matches)) {
-                            $res['requests_currently_processed'] = (int) $matches[1];
+                            $res['status']['requests_currently_processed'] = (int) $matches[1];
                         }
                     }
                     if (\str_contains($line, ' workers gracefully restarting')) {
                         if (\preg_match('/(\d+)\s+workers gracefully restarting/', $line, $matches)) {
-                            $res['workers_gracefully_restarting'] = (int) $matches[1];
+                            $res['status']['workers_gracefully_restarting'] = (int) $matches[1];
                         }
                     }
                     if (\str_contains($line, ' idle workers')) {
                         if (\preg_match('/(\d+)\s+idle workers/', $line, $matches)) {
-                            $res['idle_workers'] = (int) $matches[1];
+                            $res['status']['idle_workers'] = (int) $matches[1];
                         }
                     }
                     if (\str_contains($line, 'cache type:')) {
                         if (\preg_match('/cache type:\s+<b>([a-zA-Z0-9]+)<\/b>/', $line, $matches)) {
-                            $res['ssl_cache_type'] = $matches[1];
+                            $res['status']['ssl_cache_type'] = $matches[1];
                         }
                     }
                     if (\str_contains($line, 'shared memory:')) {
                         if (\preg_match('/shared memory:\s+<b>(\d+)<\/b>\s+([a-zA-Z]+),/', $line, $matches)) {
-                            $res['ssl_shared_memory'] = ['value' => (int) $matches[1], 'unit' => $matches[2]];
+                            $res['status']['ssl_shared_memory'] = ['value' => (int) $matches[1], 'unit' => $matches[2]];
                         }
                     }
                 }
---
+++ tests/Parser/WebServer/HttpdTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Ginfo\Tests\Parser\WebServer;
+
+use Ginfo\Parser\WebServer\Httpd;
+use PHPUnit\Framework\TestCase;
+
+final class HttpdTest extends TestCase
+{
+    public function testHttpd(): void
+    {
+        // my local config
+        $data = Httpd::work('http://localhost/openserver/server-status');
+        if (!$data) {
+            self::markTestSkipped('Httpd is not found');
+        }
+
+        self::assertStringStartsWith('2.4.', $data['version']);
+        self::assertStringStartsWith('-D', $data['args']);
+        self::assertStringStartsWith('-1.', $data['status']['load']);
+    }
+}
