570d62bf9853de0e1dba8eb0100bd1ef23cba167
Gemorroj <wapinet@mail.ru>
2025-05-04 23:06:40 +0300

    fix Elasticsearch test

--- tests/Parser/Database/ElasticsearchTest.php
+++ tests/Parser/Database/ElasticsearchTest.php
@@ -4,12 +4,16 @@ namespace Ginfo\Tests\Parser\Database;
 
 use Ginfo\Parser\Database\Elasticsearch;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpClient\HttpClient;
 
 final class ElasticsearchTest extends TestCase
 {
     public function testElasticsearch(): void
     {
-        $data = (new Elasticsearch())->run('https://127.0.0.1:9200/_cluster/stats', 'admin', 'Qdfg!_13dZ');
+        $httpClient = HttpClient::create([
+            'verify_peer' => false,
+        ]);
+        $data = (new Elasticsearch())->run('https://127.0.0.1:9200/_cluster/stats', 'admin', 'Qdfg!_13dZ', $httpClient);
 
         self::assertNotEmpty($data['stats']['cluster_name']);
         self::assertNotEmpty($data['stats']['nodes']['count']);
